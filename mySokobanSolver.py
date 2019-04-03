c44d2bc7de430478722046f6d5b78420d590ad4c
Mai98 <mkb98@live.com>
2019-04-03 12:02:10 +1000

    Added my info to the my_team function

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -35,7 +35,7 @@ def my_team():
     of triplet of the form (student_number, first_name, last_name)
 
     '''
-   return [ (9945008, 'Cody', 'Cripps'), (10283391, 'Faith', 'Lim'), (1234569, 'Eva', 'Tardos')
+    return [ (9945008, 'Cody', 'Cripps'), (10283391, 'Faith', 'Lim'), (10411551, 'Mai', 'Bernt') ]
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
