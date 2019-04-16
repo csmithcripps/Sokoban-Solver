1cd893b57228302c4e4035540459b6a8f256e720
Mai98 <mkb98@live.com>
2019-04-16 17:39:39 +1000

    Editted comments

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -368,16 +368,16 @@ class SokobanPuzzle(search.Problem):
             for box in state.boxes:
                 if can_go_there(state, box):
                     for movement in MOVEMENTS:
-                        # Apply given movement to the position of the box
+                        # Apply movement to the box
                         action = (box[0] + MOVEMENTS[movement][0], box[1] + MOVEMENTS[movement][1])
                         # If taboo cells are not allowed
                         if not self.allow_taboo_push:
                             if action in taboo_cells_positions():
                                 continue
-                        # If the action results in a wall position the action is illegal
+                        # If the action results in a wall
                         if action in state.walls:
                             continue
-                        # If the action results in another box, the action is illegal
+                        # If the action results in another box
                         if action in state.boxes:
                             continue
                         # If no constraints are violated add the box position and the movement to the list
@@ -385,20 +385,20 @@ class SokobanPuzzle(search.Problem):
             return actions
         else:
             for movement in MOVEMENTS:
-                # Apply given movement to the position of the worker
+                # Apply movement to the worker
                 action = (state.worker[0] + MOVEMENTS[movement][0], state.worker[1] + MOVEMENTS[movement][1])
                 # If taboo cells are not allowed
                 if not self.allow_taboo_push:
                     if action in taboo_cells_positions():
                         continue
-                # If the action results in a wall position the action is illegal
+                # If the action results in a wall
                 if action in state.walls:
                     continue
                 # If the action pushes a box
                 if action in state.boxes:
                     # The new position of the box
                     box_movement = (action[0] + MOVEMENTS[movement][0], action[1] + MOVEMENTS[movement][1])
-                    # If the box is pushed into a wall or another box the action is illegal
+                    # If the box is pushed into a wall or another box
                     if box_movement in state.walls or box_movement in state.boxes:
                         continue
                 # If no constraints are violated add the action to the list
